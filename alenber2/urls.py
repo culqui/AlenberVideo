66aa6396cc6ec9fe9040f50c73bd6eb878627653
Eduardo <edgame127@gmail.com>
2016-02-06 18:58:00 -0500

    fix: conflict admin url

--- alenber2/urls.py
+++ alenber2/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-	url(r'^', include('subast.urls')),
+	url(r'^app/', include('subast.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- subast/urls.py
+++ subast/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 	url(r'^(?P<company_name>[a-z]+)/order/new$', views.order_new, name = 'order_new'),
 	url(r'^(?P<company_name>[a-z]+)/order/new/unique$', views.order_new_unique, name = 'order_new_unique'),
 	url(r'^(?P<company_name>[a-z]+)/order/new/multiple$', views.order_new_multiple, name = 'order_new_multiple'),
+	url(r'^(?P<company_name>[a-z]+)/order/publish$', views.order_new_multiple, name = 'order_publish'),
 	url(r'^companys_json/$', views.companys_json, name = 'companys_json'),
 ]
